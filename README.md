566828f37880a4b65fd5a3a116e00021491bc341
Lucas F Mancan <lucasfmancan@hotmail.com>
2024-04-05 08:43:57 -0300

    Melhorando implementação teste de carga

--- README.md
+++ README.md
@@ -3,7 +3,27 @@
 
 ## Testes
 
+Usando Completable future com limitador de tempo de requisição a 4000ms:
 
+
+docker stats
+CONTAINER ID   NAME                            CPU %     MEM USAGE / LIMIT   MEM %     NET I/O           BLOCK I/O         PIDS
+ac701581683b   hello-virtual-threads-nginx-1   5.36%     29.14MiB / 128MiB   22.77%    20.4MB / 19.2MB   12.5MB / 12.3kB   2
+2083d647a3c5   hello-virtual-threads-api1-1    21.58%    547.7MiB / 550MiB   99.58%    25.8MB / 19.2MB   889MB / 908MB     1684
+
+Resultado gatling:
+
+![Texto Alternativo](https://github.com/lucasmancan/hello-virtual-threads/blob/master/spring-mvc-async.png)
+
+Usando Spring MVC padrão
+
+CONTAINER ID   NAME                            CPU %     MEM USAGE / LIMIT   MEM %     NET I/O           BLOCK I/O     PIDS
+f40659e0f10b   hello-virtual-threads-api1-1    10.36%    420.7MiB / 550MiB   76.49%    6.5MB / 3.63MB    0B / 164kB    221
+286b4e0d4627   hello-virtual-threads-nginx-1   5.80%     66.79MiB / 128MiB   52.18%    5.79MB / 5.88MB   0B / 12.3kB   2
+
+Resultado Gatling
+
+![Texto Alternativo](https://github.com/lucasmancan/hello-virtual-threads/blob/master/spring-mvc-default.png)
 
 
 Referências
--- src/main/java/dev/danvega/hellovirtualthreads/HttpBinController.java
+++ src/main/java/dev/danvega/hellovirtualthreads/HttpBinController.java
@@ -1,5 +1,7 @@
 package dev.danvega.hellovirtualthreads;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/httpbin")
@@ -20,15 +25,29 @@ public class HttpBinController {
         restClient = restClientBuilder.baseUrl("https://httpbin.org/").build();
     }
 
+    @GetMapping("/block/sync/{seconds}")
+    public String delay2(@PathVariable int seconds) {
+        ResponseEntity<Void> result = getVoidResponseEntity(seconds);
+        log.info("{} on {}", result.getStatusCode(), Thread.currentThread());
+        return Thread.currentThread().toString();
+    }
+
+
     @GetMapping("/block/{seconds}")
-    public String delay(@PathVariable int seconds) {
-        ResponseEntity<Void> result = restClient.get()
+    public CompletableFuture<String> delay(@PathVariable int seconds) {
+        return CompletableFuture.supplyAsync(() -> {
+            ResponseEntity<Void> result = getVoidResponseEntity(seconds);
+            log.info("{} on {}", result.getStatusCode(), Thread.currentThread());
+            return Thread.currentThread().toString();
+        });
+    }
+
+
+    private ResponseEntity<Void> getVoidResponseEntity(int seconds) {
+        return restClient.get()
                 .uri("/delay/" + seconds)
                 .retrieve()
                 .toBodilessEntity();
-
-        log.info("{} on {}", result.getStatusCode(), Thread.currentThread());
-        return Thread.currentThread().toString();
     }
 
 }
